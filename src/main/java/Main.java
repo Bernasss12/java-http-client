4ad6531531422a057af188e0891998a1cb366c7e
Bernardo Antunes <bernasss12@gmail.com>
2024-04-02 17:30:04 +0100

    try stage 5 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -14,55 +12,71 @@ import model.HttpResponseStatus;
 public class Main {
 
     public static void main(String[] args) {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
 
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
-
-        try {
-            serverSocket = new ServerSocket(4221);
+        try (ServerSocket serverSocket = new ServerSocket(4221);) {
             serverSocket.setReuseAddress(true);
-            System.out.println("Waiting for connection!");
-            clientSocket = serverSocket.accept(); // Wait for connection from client.
-            System.out.println("Connected!");
-            try (InputStream in = clientSocket.getInputStream()) {
-                try (OutputStream out = clientSocket.getOutputStream()) {
-                    HttpRequest request = HttpRequest.fromStream(in);
-                    if (request.path().equals("/")) {
-                        out.write(new HttpResponse(
-                                "HTTP/1.1",
-                                HttpResponseStatus.OK,
-                                null,
-                                null
-                        ).getBytes());
-                    } else if (request.path().equals("/user-agent")) {
-                        out.write(new HttpResponse(
-                                "HTTP/1.1",
-                                HttpResponseStatus.OK,
-                                HttpResponseContentType.TEXT_PLAIN,
-                                request.headers().get("User-Agent")
-                        ).getBytes());
-                    } else if (request.path().startsWith("/echo/")) {
-                        String content = request.path().replace("/echo/", "");
-                        out.write(new HttpResponse(
-                                "HTTP/1.1",
-                                HttpResponseStatus.OK,
-                                HttpResponseContentType.TEXT_PLAIN,
-                                content
-                        ).getBytes());
-                    } else {
-                        out.write(new HttpResponse(
-                                "HTTP/1.1",
-                                HttpResponseStatus.NOT_FOUNT,
-                                null,
-                                null
-                        ).getBytes());
-                    }
-                }
+            System.out.println("Waiting for connections!");
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
+                new Thread(
+                        () -> handleClient(clientSocket)
+                ).start();
             }
+
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
     }
+
+    private static void handleClient(Socket clientSocket) {
+        System.out.println("Connected!");
+        try (InputStream in = clientSocket.getInputStream()) {
+            try (OutputStream out = clientSocket.getOutputStream()) {
+                HttpRequest request = HttpRequest.fromStream(in);
+                if (request.path().equals("/")) {
+                    out.write(new HttpResponse(
+                            "HTTP/1.1",
+                            HttpResponseStatus.OK,
+                            null,
+                            null
+                    ).getBytes());
+                } else if (request.path().equals("/user-agent")) {
+                    out.write(new HttpResponse(
+                            "HTTP/1.1",
+                            HttpResponseStatus.OK,
+                            HttpResponseContentType.TEXT_PLAIN,
+                            request.headers().get("User-Agent")
+                    ).getBytes());
+                } else if (request.path().startsWith("/echo/")) {
+                    String content = request.path().replace("/echo/", "");
+                    out.write(new HttpResponse(
+                            "HTTP/1.1",
+                            HttpResponseStatus.OK,
+                            HttpResponseContentType.TEXT_PLAIN,
+                            content
+                    ).getBytes());
+                } else {
+                    out.write(new HttpResponse(
+                            "HTTP/1.1",
+                            HttpResponseStatus.NOT_FOUNT,
+                            null,
+                            null
+                    ).getBytes());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println("Disconnected!");
+            }
+        }
+    }
+
 }
