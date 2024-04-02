bd5090eccaccd23f49223ef6739baecc5a2e96cb
Bernardo Antunes <bernasss12@gmail.com>
2024-04-02 15:02:38 +0100

    try stage 2 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,8 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,7 +18,17 @@ public class Main {
         try {
             serverSocket = new ServerSocket(4221);
             serverSocket.setReuseAddress(true);
+            System.out.println("Waiting for connection!");
             clientSocket = serverSocket.accept(); // Wait for connection from client.
+            System.out.println("Connected!");
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                try (OutputStream out = clientSocket.getOutputStream()) {
+                    in.readLine(); // Ignore the client input
+                    System.out.println("Input was read from client");
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    System.out.println("Output was sent to client");
+                }
+            }
             System.out.println("accepted new connection");
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
